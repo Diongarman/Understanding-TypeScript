fc74a236d1fbc294326ecfff448b3251b5279534
Dion Garman <diongarman@gmail.com>
2019-12-27 20:26:28 +0000

    Enum types - using globals is a common pattern in JS but in TS role is inferred to be just a number and can be assigned meaningless values in client code

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Dion',
+//     age: 100,
+//     hobbies: ['Sports', 'Science'],
+//     role: [2, 'author']
+// };
+//global constants
+var ADMIN = 0;
+var READ_ONLY = 1;
+var AUTHOR = 2;
 var person = {
     name: 'Dion',
     age: 100,
     hobbies: ['Sports', 'Science'],
-    role: [2, 'author']
+    role: ADMIN
 };
 //problematic code
-person.role.push('unwanted value');
+//person.role.push('unwanted value');
 //person.role[1] = 69;
+if (person.role === ADMIN) {
+    console.log("is admin");
+}
 var favActivities;
 favActivities = ['sports'];
 console.log(person.age);
--- app.ts
+++ app.ts
@@ -1,18 +1,36 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Dion',
+//     age: 100,
+//     hobbies: ['Sports', 'Science'],
+//     role: [2, 'author']
+// };
+
+//global constants
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+
+const person = {
     name: 'Dion',
     age: 100,
     hobbies: ['Sports', 'Science'],
-    role: [2, 'author']
+    role: ADMIN
 };
 
+
 //problematic code
-person.role.push('unwanted value');
+//person.role.push('unwanted value');
 //person.role[1] = 69;
+
+if (person.role === ADMIN) {
+
+    console.log("is admin");
+}
 
 
 let favActivities: string[];
