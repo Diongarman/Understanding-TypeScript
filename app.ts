8227e52cc4adb2be1ed64d0120783134eddebe33
Dion Garman <diongarman@gmail.com>
2019-12-27 19:00:16 +0000

    Using Types - declaring types explitly, this example causes compile error[correction]

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     
 }
 
-const number1: number;
+let number1: number;
 number1 = '5';
 const number2 = 2.8;
 const showResult = true;
