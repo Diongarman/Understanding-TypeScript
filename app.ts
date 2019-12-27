91acf9c86a1b0108164f3918984aa47bc91101ff
Dion Garman <diongarman@gmail.com>
2019-12-27 20:08:24 +0000

    Tuple types - explicit typing fixes inference so tuples work as intended

--- app.ts
+++ app.ts
@@ -1,13 +1,20 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Dion',
     age: 100,
     hobbies: ['Sports', 'Science'],
     role: [2, 'author']
 };
 
-
+//problematic code
 person.role.push('unwanted value');
 person.role[1] = 69;
+
+
 let favActivities: string[];
 
 favActivities = ['sports'];
