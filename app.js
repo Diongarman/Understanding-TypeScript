2d8d9cd50a3a3038faf39d510f5f6dbc39cfa804
Dion Garman <diongarman@gmail.com>
2019-12-27 19:16:31 +0000

    Object types - compilation error for trying to access key that d.n.e

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1;
-number1 = 5;
-var number2 = 2.8;
-var showResult = true;
-var printPhrase = "The number is... ";
-printPhrase = 69;
-var result = add(number1, number2, showResult, printPhrase);
-console.log(result);
+var person = {
+    name: 'Dion',
+    age: 100
+};
+console.log(person.nickname);
