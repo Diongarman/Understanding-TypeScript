97f82b52bfac2f0fd9fdff3ade55037ae810acd9
Dion Garman <diongarman@gmail.com>
2019-12-27 19:25:02 +0000

    Object types - object types can be created for nested objects too

--- app.ts
+++ app.ts
@@ -1,6 +1,18 @@
-const person = {
-    name: 'Dion',
-    age: 100
-};
+const product: {
+    id: string;
+    price: number;
+    tags: string[],
+    details: {
+      title: string;
+      description: string;
+    }
+  } = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+      title: 'Red Carpet',
+      description: 'A great carpet - almost brand-new!'
+    }
+  }
 
-console.log(person.age);
