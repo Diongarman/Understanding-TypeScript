70a1622b2c6679a0be7c503ca0b5c9787518e0f6
Dion Garman <diongarman@gmail.com>
2019-12-28 14:16:58 +0000

    Union types - allows multiple types to be passed in. [added runtime checks so compilation is successful]

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string];
-// } = {
-//     name: 'Dion',
-//     age: 100,
-//     hobbies: ['Sports', 'Science'],
-//     role: [2, 'author']
-// };
-//global constants
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'Dion',
-    age: 100,
-    hobbies: ['Sports', 'Science'],
-    role: Role.AUTHOR
-};
-//problematic code
-//person.role.push('unwanted value');
-//person.role[1] = 69;
-if (person.role === Role.AUTHOR) {
-    console.log("is admin");
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-var favActivities;
-favActivities = ['sports'];
-console.log(person.age);
+var combineAges = combine(12, 13);
+console.log(combineAges);
+var combineNames = combine('Daniel', 'Hernandez');
+console.log(combineNames);
