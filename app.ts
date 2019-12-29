f12825d9bbaa1d726e3160175aecb44c0b312077
Dion Garman <diongarman@gmail.com>
2019-12-29 17:24:29 +0000

    The 'never' Type - value is a never produced, script is always crashed

--- app.ts
+++ app.ts
@@ -7,3 +7,8 @@ userInput = 'Mikey';
 if (typeof userInput === "string") {
     userName = userInput;
 }
+
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+    }
