6bbfe5a81f551a399f2f074f0b61ac0243a1e3a7
Dion Garman <diongarman@gmail.com>
2019-12-29 16:51:25 +0000

    Function Types & Callbacks - can specify callbacks params and return

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ function add(n1, n2) {
 function printResult(num) {
     console.log(num);
 }
-//printResult(add(11,58))
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 var combineValues;
 combineValues = add;
-//below line will throw compilation error 
-//combineValues = 5;
-console.log(printResult(add(11, 58)));
+//below line will throw compilation error (CErr)
+//combineValues = printResult;
+addAndHandle(6, 9, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -6,16 +6,18 @@ function printResult(num: number): void {
     console.log(num);
 } 
 
-//printResult(add(11,58))
-
+function addAndHandle(n1: number, n2: number, cb: (a: number) =>  void){
+    const result = n1 + n2;
+    cb(result)
+}
 
 let combineValues: (a:number, b:number) => number;
 
 combineValues = add;
 //below line will throw compilation error (CErr)
-combineValues = printResult;
+//combineValues = printResult;
 
-//line below will throw a CErr though as combineValue can be cast to any 'Function' type
-
-console.log(printResult(add(11,58)));
+addAndHandle(6, 9, (result) => {
+    console.log(result);
+})
 
